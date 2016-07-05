0e95867f6e7bb217344b300e6b9537db9a381269
Clare Kang <shiningkey89@gmail.com>
2016-07-06 00:46:49 +0900

    set layout with material-ui

--- source/entry.jsx
+++ source/entry.jsx
@@ -1,17 +1,11 @@
 import './entry.scss';
+
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, IndexRedirect,
-  hashHistory, applyRouterMiddleware } from 'react-router';
+import { Router, Route, IndexRoute, IndexRedirect, hashHistory, applyRouterMiddleware } from 'react-router';
 import useScroll from 'react-router-scroll';
-
-
-
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 // Redux
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -21,42 +15,59 @@ import thunk from 'redux-thunk';
 
 import rootReducer from 'redux-modules/reducer';
 
+// app components
+import RootContainer from './components/RootContainer';
+
+// material ui
+// import {cyan500} from 'material-ui/styles/colors';
+import customTheme from 'library/customTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 // IE Check
 function msieversion() {
   const ua = window.navigator.userAgent;
   const msie = ua.indexOf('MSIE ');
-  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
-  {
+  // If Internet Explorer, return version number
+  if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
     return 1;
   }
   return -1;
 }
-
 // Info text for IEs
 if (msieversion() > 0) {
-  window.document.write('<p> 현재 Internet Explorer 는 지원하지 않습니다. Chrome을 사용해 주세요! </p>');
+  window.document.write('<p> 현재 Internet Explorer 는 지원하지 않습니다. Chrome을 사용해주세요! </p>');
 } else {
   const store = createStore(
     rootReducer, compose(applyMiddleware(thunk))
   );
-
   const history = syncHistoryWithStore(hashHistory, store);
 
-  const requireAuth = (nextState, replace) => {
-    if (!localStorage.getItem('authData')) {
-      replace({
-        pathname: '/login',
-        state: { nextPathname: nextState.location.pathname },
-      });
-    } else {
-      // store.dispatch(success());
-    }
-  };
+
+
+
 
   ReactDOM.render(
-    <div>
-      Hello World!
-    </div>
+    <MuiThemeProvider muiTheme={getMuiTheme(customTheme)}>
+      <Provider store={store}>
+        <Router history={history} render={applyRouterMiddleware(useScroll())}>
+          <Route path="/" component={RootContainer}>
+          {/*
+            <Route path="live" component={LiveContainer} onEnter={requireAuth}>
+              <IndexRedirect to="/live/deliveries" />
+              <Route path="deliveries" component={DeliveryContainer}>
+                <IndexRoute component={ConnectedDeliveryList} />
+                <Route path=":deliveryId" component={ConnectedDeliveryDetail} />
+              </Route>
+            </Route>
+          */}
+          </Route>
+        </Router>
+      </Provider>
+    </MuiThemeProvider>
   , document.getElementById('app'));
 }
